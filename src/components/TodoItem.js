5bf09a72d30d777b8fa5636e770a1e5a6c92cbc5
rai8 <hytonne@outlook.com>
2019-08-14 19:05:35 +0300

    functional styling and creating checkboxes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,6 +4,14 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 export class TodoItem extends Component {
   getStyle = () => {
+    return {
+      background: "#f4f4f4",
+      padding: "10px",
+      borderBottom: "1px #ccc dotted",
+      textDecoration: this.props.todo.completed ? "line-through" : "none"
+    };
+  };
+  /*  getStyle = () => {
     if (this.props.todo.completed) {
       return {
         textDecoration: "line-through"
@@ -13,11 +21,17 @@ export class TodoItem extends Component {
         textDecoration: "none"
       };
     }
+  }; */
+  markComplete = e => {
+    console.log(this.props);
   };
   render() {
     return (
       <div style={this.getStyle()}>
-        <p> {this.props.todo.title}</p>
+        <p>
+          <input type="checkbox" onChange={this.markComplete} />
+          {this.props.todo.title}
+        </p>
       </div>
     );
   }

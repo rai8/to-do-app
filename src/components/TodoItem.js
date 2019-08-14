0116fed631745546aa48dea6eb6ddfb6bf4b39e3
rai8 <hytonne@outlook.com>
2019-08-14 18:36:25 +0300

    propTypes validation

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,18 @@
 //press RCE to automatically generate your component, that is if you have es7, react---- installed
 
 import React, { Component } from "react";
-
+import PropTypes from "prop-types";
 export class TodoItem extends Component {
   render() {
     return (
       <div>
-        <p> Hello Rhytonne</p>
+        <p> {this.props.todo.title}</p>
       </div>
     );
   }
 }
-
+//proptypes
+TodoItem.propTypes = {
+  todo: PropTypes.object.isRequired
+};
 export default TodoItem;
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -2,10 +2,14 @@
 
 import React, { Component } from "react";
 import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 class Todos extends Component {
   render() {
-    return this.props.todos.map(todo => <TodoItem />);
+    return this.props.todos.map(todo => <TodoItem key={todo.id} todo={todo} />);
   }
 }
-
+//proptypes
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired
+};
 export default Todos;

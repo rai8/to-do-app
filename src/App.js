d62a70b848e4b861d03bca5d0eb4a08ed39d8e60
rai8 <hytonne@outlook.com>
2019-08-14 22:02:15 +0300

    setState and generating random ids

--- src/App.js
+++ src/App.js
@@ -6,22 +6,23 @@ import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
 import AddTodo from "./components/AddTodo";
 import "./App.css";
+import uuid from "uuid";
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: "take out the trash",
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: "Dinner with my crush",
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: "Meeting with boss",
         completed: false
       }
@@ -46,12 +47,22 @@ class App extends Component {
       todos: [...this.state.todos.filter(todo => todo.id !== id)]
     });
   };
+  //Add to do
+  addTodo = title => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    };
+    this.setState({ todos: [...this.state.todos, newTodo] });
+  };
+
   render() {
     return (
       <div className="App">
         <div className="container">
           <Header />
-          <AddTodo />
+          <AddTodo addTodo={this.addTodo} />
           <Todos
             todos={this.state.todos}
             markComplete={this.markComplete}

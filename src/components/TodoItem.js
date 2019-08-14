6dbfc8ab7ee12ffff64eac0c9f1e10214a7ee622
rai8 <hytonne@outlook.com>
2019-08-14 18:43:24 +0300

    styling, instyle and using variables

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export class TodoItem extends Component {
   render() {
     return (
-      <div>
+      <div style={itemStyle}>
         <p> {this.props.todo.title}</p>
       </div>
     );
@@ -14,5 +14,9 @@ export class TodoItem extends Component {
 //proptypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+};
+
+const itemStyle = {
+  backgroundColor: "#f4f4f4"
 };
 export default TodoItem;
